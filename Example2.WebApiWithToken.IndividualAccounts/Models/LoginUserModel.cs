247663ae813269e48a27961f0d0a74fce22ef767
Jon P Smith <jon.smith@selectiveanalytics.com>
2021-09-06 15:52:14 +0100

    Added required attributes

--- Example2.WebApiWithToken.IndividualAccounts/Models/LoginUserModel.cs
+++ Example2.WebApiWithToken.IndividualAccounts/Models/LoginUserModel.cs
@@ -1,11 +1,16 @@
 ﻿// Copyright (c) 2021 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
 // Licensed under MIT license. See License.txt in the project root for license information.
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Example2.WebApiWithToken.IndividualAccounts.Models
 {
     public class LoginUserModel
     {
+        [Required(AllowEmptyStrings = false)]
         public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
         public string Password { get; set; }
     }
 }
