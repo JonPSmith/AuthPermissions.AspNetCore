a3a4c6cde8734a3609ea1a293be4ae5bc460cec7
Jon P Smith <jon.smith@selectiveanalytics.com>
2022-01-20 14:30:17 +0000

    Added exception if entity doesn't have a DataKey

--- Example4.ShopCode/EfCoreCode/RetailDbContext.cs
+++ Example4.ShopCode/EfCoreCode/RetailDbContext.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) 2021 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
 // Licensed under MIT license. See License.txt in the project root for license information.
 
+using System;
 using AuthPermissions.AspNetCore.GetDataKeyCode;
-using AuthPermissions.AspNetCore.Services;
 using AuthPermissions.CommonCode;
 using AuthPermissions.DataLayer.EfCode;
 using Example4.ShopCode.EfCoreClasses;
@@ -35,6 +35,11 @@ namespace Example4.ShopCode.EfCoreCode
                 if (typeof(IDataKeyFilterReadOnly).IsAssignableFrom(entityType.ClrType))
                 {
                     entityType.AddHierarchicalTenantReadOnlyQueryFilter(this);
+                }
+                else
+                {
+                    throw new Exception(
+                        $"You haven't added the {nameof(IDataKeyFilterReadOnly)} to the entity {entityType.ClrType.Name}");
                 }
 
                 foreach (var mutableProperty in entityType.GetProperties())
