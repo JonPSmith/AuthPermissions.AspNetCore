215b385e6c9ec7158a95b926999e1e1033d8c2f2
Jon P Smith <jon.smith@selectiveanalytics.com>
2022-06-13 14:48:18 +0100

    Undo Pull request #43

--- AuthPermissions.BaseCode/DataLayer/Classes/Tenant.cs
+++ AuthPermissions.BaseCode/DataLayer/Classes/Tenant.cs
@@ -124,11 +124,10 @@ namespace AuthPermissions.BaseCode.DataLayer.Classes
         /// </summary>
         public IReadOnlyCollection<Tenant> Children => _children?.ToList();
 
-
         /// <summary>
         /// This holds any Roles that have been specifically 
         /// </summary>
-        public IReadOnlyCollection<RoleToPermissions> TenantRoles => _tenantRoles?.ToList() ?? new List<RoleToPermissions>();
+        public IReadOnlyCollection<RoleToPermissions> TenantRoles => _tenantRoles?.ToList();
         /// <summary>
         /// Easy way to see the tenant and its key
         /// </summary>
