96c6a2612abdc87459da86bc81494a8501f041e2
Idan <ghsijns@gmail.com>
2022-06-27 00:12:25 +0300

    Added minimal api fluent implementation of HasPermissionAttribute

---
+++ AuthPermissions.AspNetCore/PolicyCode/PermissionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthPermissions.AspNetCore.PolicyCode
+{
+    /// <summary>
+    /// Class which contains extensions for the ASP.Net core permission policy
+    /// </summary>
+    public static class PermissionExtensions
+    {
+        /// <summary>
+        /// The fluent minimal api implementation of <see cref="HasPermissionAttribute"/>
+        /// </summary>
+        /// <typeparam name="TBuilder"></typeparam>
+        /// <param name="builder"></param>
+        /// <param name="permission"></param>
+        /// <returns></returns>
+        public static TBuilder HasPermission<TBuilder>(this TBuilder builder, object permission) where TBuilder : IEndpointConventionBuilder
+        {
+            var authorizeData = new HasPermissionAttribute(permission);
+            builder.RequireAuthorization(authorizeData);
+
+            return builder;
+        }
+    }
+}
---
+++ Test/TestHelpers/StubEndpointBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.TestHelpers
+{
+    public class StubEndpointBuilder : EndpointBuilder
+    {
+        public override Endpoint Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
