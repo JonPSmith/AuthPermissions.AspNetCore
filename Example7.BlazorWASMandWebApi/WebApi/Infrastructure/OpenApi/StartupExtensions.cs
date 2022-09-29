4606350dd6a7e6bbea2535f15f3c4d5c33864799
akema-trebla <joshmcgrath7@gmail.com>
2022-09-29 12:29:06 +0000

    Switch out Swashbuckle for Nswag

---
+++ Example7.BlazorWASMandWebApi/WebApi/Infrastructure/OpenApi/StartupExtensions.cs
@@ -0,0 +1,127 @@
+﻿
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NSwag;
+using NSwag.AspNetCore;
+using NSwag.Generation.Processors.Security;
+using ZymLabs.NSwag.FluentValidation;
+
+namespace Example7.BlazorWASMandWebApi.Infrastructure.OpenApi;
+
+public static class StartupExtensions
+{
+    internal static IServiceCollection AddOpenApiDocumentation(this IServiceCollection services, IConfiguration config)
+    {
+        var settings = config.GetSection(nameof(SwaggerSettings)).Get<SwaggerSettings>();
+        if (settings.Enable)
+        {
+            services.AddVersionedApiExplorer(o => o.SubstituteApiVersionInUrl = true);
+            services.AddEndpointsApiExplorer();
+            services.AddOpenApiDocument((document, serviceProvider) =>
+            {
+                document.PostProcess = doc =>
+                {
+                    doc.Info.Title = settings.Title;
+                    doc.Info.Version = settings.Version;
+                    doc.Info.Description = settings.Description;
+                    doc.Info.Contact = new()
+                    {
+                        Name = settings.ContactName,
+                        Email = settings.ContactEmail,
+                        Url = settings.ContactUrl
+                    };
+                    doc.Info.License = new()
+                    {
+                        Name = settings.LicenseName,
+                        Url = settings.LicenseUrl
+                    };
+                };
+
+                if (config["SecuritySettings:Provider"].Equals("AzureAd", StringComparison.OrdinalIgnoreCase))
+                {
+                    document.AddSecurity(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+                    {
+                        Type = OpenApiSecuritySchemeType.OAuth2,
+                        Flow = OpenApiOAuth2Flow.AccessCode,
+                        Description = "OAuth2.0 Auth Code with PKCE",
+                        Flows = new()
+                        {
+                            AuthorizationCode = new()
+                            {
+                                AuthorizationUrl = config["SecuritySettings:Swagger:AuthorizationUrl"],
+                                TokenUrl = config["SecuritySettings:Swagger:TokenUrl"],
+                                Scopes = new Dictionary<string, string>
+                                {
+                                    { config["SecuritySettings:Swagger:ApiScope"], "access the api" }
+                                }
+                            }
+                        }
+                    });
+                }
+                else
+                {
+                    document.AddSecurity(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+                    {
+                        Name = "Authorization",
+                        Description = "Input your Bearer token to access this API",
+                        In = OpenApiSecurityApiKeyLocation.Header,
+                        Type = OpenApiSecuritySchemeType.Http,
+                        Scheme = JwtBearerDefaults.AuthenticationScheme,
+                        BearerFormat = "JWT",
+                    });
+                }
+
+                document.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor());
+                document.OperationProcessors.Add(new SwaggerGlobalAuthProcessor());
+
+                document.OperationProcessors.Add(new SwaggerHeaderAttributeProcessor());
+
+                var fluentValidationSchemaProcessor = serviceProvider.CreateScope().ServiceProvider.GetService<FluentValidationSchemaProcessor>();
+                document.SchemaProcessors.Add(fluentValidationSchemaProcessor);
+            });
+
+            services.AddScoped(provider =>
+            {
+                var validationRules = provider.GetService<IEnumerable<FluentValidationRule>>();
+                var loggerFactory = provider.GetService<ILoggerFactory>();
+
+                return new FluentValidationSchemaProcessor(provider, validationRules, loggerFactory);
+            });
+        }
+
+        return services;
+    }
+
+    internal static IApplicationBuilder UseOpenApiDocumentation(this IApplicationBuilder app, IConfiguration config)
+    {
+        if (config.GetValue<bool>("SwaggerSettings:Enable"))
+        {
+            app.UseOpenApi();
+            app.UseSwaggerUi3(options =>
+            {
+                options.DefaultModelsExpandDepth = -1;
+                options.DocExpansion = "none";
+                options.TagsSorter = "alpha";
+                options.AdditionalSettings.Add("displayRequestDuration", true);
+                if (config["SecuritySettings:Provider"].Equals("AzureAd", StringComparison.OrdinalIgnoreCase))
+                {
+                    options.OAuth2Client = new OAuth2ClientSettings
+                    {
+                        AppName = "Full Stack Hero Api Client",
+                        ClientId = config["SecuritySettings:Swagger:OpenIdClientId"],
+                        ClientSecret = string.Empty,
+                        UsePkceWithAuthorizationCodeGrant = true,
+                        ScopeSeparator = " "
+                    };
+                    options.OAuth2Client.Scopes.Add(config["SecuritySettings:Swagger:ApiScope"]);
+                }
+            });
+        }
+
+        return app;
+    }
+}
+
