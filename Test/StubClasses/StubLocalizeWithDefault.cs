43417fc0cac9dfca00a91eac3367f1a7e32ac999
Jon P Smith <jon.smith@selectiveanalytics.com>
2022-11-28 14:52:43 +0000

    Handles generic calling class

--- Test/StubClasses/StubLocalizeWithDefault.cs
+++ Test/StubClasses/StubLocalizeWithDefault.cs
@@ -59,7 +59,7 @@ public class StubLocalizeWithDefault<TResource> : ILocalizeWithDefault<TResource
             return;
 
         var localizeKey = localizeKeyData.LocalizeKey ?? "already localize";
-        var callingClassName = localizeKeyData.CallingClass.Name;
+        var callingClassName = GetFormattedName(localizeKeyData.CallingClass);
 
         //This will hold any existing database entries that have the same ResourceFile and LocalizeKey
         var sameLocalizationReference = context.LocalizedData
@@ -93,7 +93,28 @@ public class StubLocalizeWithDefault<TResource> : ILocalizeWithDefault<TResource
         context.SaveChanges();
     }
 
-    //-----------------------------------------------------------
+    ///thanks to https://stackoverflow.com/a/66604069/1434764
+    /// <summary>
+    /// Returns the type name. If this is a generic type, appends
+    /// the list of generic type arguments between angle brackets.
+    /// (Does not account for embedded / inner generic arguments.)
+    /// </summary>
+    /// <param name="type">The type.</param>
+    /// <returns>System.String.</returns>
+    public static string GetFormattedName(Type type)
+    {
+        if (type.IsGenericType)
+        {
+            string genericArguments = type.GetGenericArguments()
+                .Select(x => x.Name)
+                .Aggregate((x1, x2) => $"{x1}, {x2}");
+            return $"{type.Name.Substring(0, type.Name.IndexOf("`"))}"
+                   + $"<{genericArguments}>";
+        }
+        return type.Name;
+    }
+
+        //-----------------------------------------------------------
     //database part
 
     public LocalizationCaptureDb GetLocalizationCaptureDbInstance(bool turnOnManually = false)
